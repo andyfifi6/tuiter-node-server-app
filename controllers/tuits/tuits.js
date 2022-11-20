faeefe8800bd483239067024ac50d01014b7a9e0
andyfifi6 <andyfifi6@gmail.com>
2022-11-20 15:52:42 -0800

    update dislike

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -5,7 +5,7 @@ export default [
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true, "likes": 2345,
-        "disliked": false, "dislikes": 0,
+        "disliked": true, "dislikes": 1,
         "replies": 123, "retuits": 432
     },
     {
